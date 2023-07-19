f3782c3865521278dad5080c6c8354c44309fc6e
wx_orange <wx_orange@orange007.com>
2023-07-19 15:20:51 +0800

    docs: 更新md文档

--- src/hooks/useScalePage.ts
+++ src/hooks/useScalePage.ts
@@ -26,19 +26,23 @@ export default function useScalePage(option: ScaleOptions) {
     let targetRatio = option.targetRatio || 16 / 9; // 宽高比率
 
     // 2.拿到当前设备(浏览器)的宽度
-    let currentX = document.documentElement.clientWidth || document.body.clientWidth;
-    let currentY = document.documentElement.clientHeight || document.body.clientHeight;
+    let currentX =
+      document.documentElement.clientWidth || document.body.clientWidth;
+    let currentY =
+      document.documentElement.clientHeight || document.body.clientHeight;
 
     // 3.计算缩放比例
     let scaleRatio = currentX / targetX; // 参照宽度进行缩放 ( 默认情况 )
     let currentRatio = currentX / currentY; // 宽高比率
 
     if (currentRatio > targetRatio) {
-      // 4.如果当前设备的宽高比率大于设计稿的宽高比率（太宽了）,那么就以高度为参照进行缩放，并且居中显示
+      // 4.如果当前设备的宽高比率大于设计稿的宽高比率（太宽了）
+      // 那么就以高度为参照进行缩放，并且居中显示
       scaleRatio = currentY / targetY; // 参照高度进行缩放
       document.body.style.cssText = `width:${targetX}px; height:${targetY}px; transform: scale(${scaleRatio}) translateX(-50%); left: 50%`;
     } else {
-      // 4.如果当前设备的宽高比率小于或者等于设计稿的宽高比率，则开始缩放网页
+      // 4.如果当前设备的宽高比率小于或者等于设计稿的宽高比率
+      // 则正常开始缩放网页
       document.body.style.cssText = `width:${targetX}px; height:${targetY}px; transform: scale(${scaleRatio})`;
     }
   }
